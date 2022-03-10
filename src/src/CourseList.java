d1d167f2eb036a2013d2e17ab7405d78aad803c0
DavisGaunt <51679585+DavisGaunt@users.noreply.github.com>
2022-03-10 11:46:46 -0500

    prep for merge to main

---
+++ src/src/CourseList.java
@@ -0,0 +1,22 @@
+/**
+ * CourseList
+ * This class imports a list of courses and is used for selecting & loading a course
+ *
+ * @author Braden McGee
+ */
+package src;
+// Imports
+import java.util.*;
+
+public class CourseList {
+    // Vars
+    private Vector<String> courses;
+    private Vector<String> filepaths;
+
+    // Constructor
+    public CourseList(String fileName){
+        courses = new Vector<String>();
+        filepaths = new Vector<String>();
+        
+    }
+}
