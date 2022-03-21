96c1e95fa551cc2344bc4c7da4be55c83c949ccd
braden0081 <97906030+braden0081@users.noreply.github.com>
2022-03-21 04:03:17 -0500

    Extra function setScore in student for better data handling

--- src/src/Course.java
+++ src/src/Course.java
@@ -24,6 +24,8 @@ public class Course {
         float[] catScores = new float[gradebook.getCatID().size()];
         float[] catMaxScores = new float[gradebook.getCatID().size()];
         float overallGrade = 0.0F;
+        float excludedWeight = 0.0F;
+        float totalWeight = 0.0F;
         // Loop to compute category scores
         for(int i=0; i<gradebook.getCatID().size(); i++){
             // Initializing values to 0
@@ -37,10 +39,16 @@ public class Course {
                 }
             }
             // Adding to Overall Grade: first percentile for category is calculated, then modified by weight and added to overall
-            overallGrade += ((catScores[i]/catMaxScores[i])*gradebook.getCatWeights().get(i));
-            // Setting Overall Grade with assumption that it is the last entry in Scores
-            roster.get(StudentID).setScore(roster.get(StudentID).getScores().size()-1, overallGrade);
+            if(catMaxScores[i]==0)
+                excludedWeight += gradebook.getCatWeights().get(i);
+            else
+                overallGrade += ((catScores[i]/catMaxScores[i])*gradebook.getCatWeights().get(i));
+            totalWeight += gradebook.getCatWeights().get(i);
         }
+        // Finalizing Overall Grade
+        overallGrade = overallGrade*(1/(totalWeight-excludedWeight))*100;
+        // Setting Overall Grade with assumption that it is the last entry in Scores
+        roster.get(StudentID).setScore(roster.get(StudentID).getScores().size()-1, overallGrade);
     }
 
     public void setScore(int StudentID, int AssignmentID, float score){
