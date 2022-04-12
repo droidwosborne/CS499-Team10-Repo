9980ce9ee63f2af4aa074f2283beff64d575d165
DavisGaunt <51679585+DavisGaunt@users.noreply.github.com>
2022-04-12 05:36:30 -0700

    pasword system added

--- src/src/CourseList.java
+++ src/src/CourseList.java
@@ -8,12 +8,17 @@ package src;
 // Imports
 import java.util.*;
 import src.SaveHandling.ReadSave;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class CourseList {
     // Vars
     private Vector<String> courses;
     private String teacherLName;
     private String teacherFName;
+    private String userName;
+    private String pwordHash;
 
     // Constructor
     public CourseList(){
@@ -24,11 +29,41 @@ public class CourseList {
         
         teacherLName = temp.get(0);
         teacherFName = temp.get(1);
+        pwordHash = temp.get(2);
+        userName = temp.get(3);
         
         courses = new Vector<String>();
         
-        for (int i = 2; i < temp.size(); i++)
+        for (int i = 4; i < temp.size(); i++)
             courses.add(temp.get(i));
+        
+    }
+    
+    public CourseList(String pRaw, String uName){
+        
+        ReadSave tIn = new ReadSave();
+        
+        Vector<String> temp = new Vector<String>(tIn.teacherIn());
+        
+        try {
+            pRaw = hash(pRaw);
+        }
+        catch(NoSuchAlgorithmException e){
+            throw new RuntimeException(e);
+        }
+        if(pRaw.equals(temp.get(2)) == true && uName.equals(temp.get(3)) == true){
+            
+            teacherLName = temp.get(0);
+            teacherFName = temp.get(1);
+            pwordHash = temp.get(2);
+            userName = temp.get(3);
+
+            courses = new Vector<String>();
+
+            for (int i = 4; i < temp.size(); i++)
+                courses.add(temp.get(i));
+            
+        }
         
     }
 
@@ -55,6 +90,39 @@ public class CourseList {
     
     public void setTeacherFName(String fName){
         teacherFName = fName;
+    }
+    
+    public void setPassword(String pRaw){
+        try {
+            pwordHash = hash(pRaw);
+        }
+        catch(NoSuchAlgorithmException e){
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public String getPassword(){
+        return pwordHash;
+    } 
+    
+    public void setUserName(String uName){
+        this.userName = uName;
+    }
+    
+    public String getUserName(){
+        return userName;
+    }
+    
+    
+    private String hash(String hTarget)throws NoSuchAlgorithmException{
+    
+        MessageDigest md = MessageDigest.getInstance("SHA-512");
+        byte[] messageDigest = md.digest(hTarget.getBytes());
+        BigInteger no = new BigInteger(1, messageDigest);
+        String hashtext = no.toString(16);
+
+        return hashtext;
+    
     }
     
 }
