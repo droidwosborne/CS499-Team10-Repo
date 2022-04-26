069fb99f6e47e404a56bcc416813eab5086b8191
droidwosborne <wosborne636@gmail.com>
2022-04-26 10:00:22 -0500

    Removal of Page 2
    comments out page 2 items to use courseCList

--- src/src/Main.java
+++ src/src/Main.java
@@ -82,7 +82,7 @@ public class Main {
         course1.calculateOverallGrade(2);
         WriteSave writeSave= new WriteSave();
         try {
-            writeSave.saveCourse(course1, "course1");
+            writeSave.saveCourse(course1, "class1");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -131,7 +131,7 @@ public class Main {
         course2.calculateOverallGrade(2);
         WriteSave writeSave= new WriteSave();
         try {
-            writeSave.saveCourse(course2, "course2");
+            writeSave.saveCourse(course2, "class2");
         } catch (IOException e) {
             e.printStackTrace();
         }
